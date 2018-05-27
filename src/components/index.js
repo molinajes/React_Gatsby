b8cb17bf3365ef45a3762dffe7f084b5c146e6a0
webdevcrazier <devcrazier@gmail.com>
2018-05-28 06:56:36 +0800

    add index component

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Error from './Error'
+import Section from './Section'
+import Container from './Container'
+import MenuLeft from './MenuLeft'
+import MenuTop from './MenuTop'
+
+export {
+  Error,
+  Section,
+  Container,
+  MenuLeft,
+  MenuTop,
+}
