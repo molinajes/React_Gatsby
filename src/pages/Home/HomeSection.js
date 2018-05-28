da5fa78c47297708026dca206c1acbf64668fb18
webdevcrazier <devcrazier@gmail.com>
2018-05-29 05:32:19 +0800

    add homesections

---
+++ src/pages/Home/HomeSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Section, Container } from 'components'
+
+const HomeSection = () => (
+  <Section home>
+    <Container white>
+      <h1>Konnexion propulse votre entreprise par l’innovation, le Web &amp; les Apps</h1>
+      <p>Nous sommes spécialisés dans la conception de site web, d’application Web &amp; mobile à&nbsp; Québec. Nous livrons les meilleures solutions numériques adaptées à la réalité d’affaires de votre entreprise.</p>
+    </Container>
+  </Section>
+)
+
+export default HomeSection;
