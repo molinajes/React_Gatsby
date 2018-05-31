564c396b9a8050d326bc14010876205ab3d4e5ef
webdevcrazier <devcrazier@gmail.com>
2018-05-31 10:28:43 +0800

    change index

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,20 @@
+import { Link } from 'react-router-dom'
+
 import Error from './Error'
 import Section from './Section'
 import Container from './Container'
 import MenuLeft from './MenuLeft'
 import MenuTop from './MenuTop'
+import Menu from './Menu'
+import SocialLinks from './SocialLinks'
 
 export {
+  Link,
   Error,
   Section,
   Container,
   MenuLeft,
   MenuTop,
+  Menu,
+  SocialLinks,
 }
