fe76619b5644b77c8346b24db109aad53a6a4c5b
webdevcrazier <devcrazier@gmail.com>
2018-06-03 13:34:10 +0800

    add layout

---
+++ src/layouts/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import './index.css'
+
+const Layout = ({ children, data }) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+    >
+      <meta charSet='utf-8' />
+      <meta name='description' content='Konnexion' />
+      <link rel='manifest' href='/manifest.json' />
+      <link rel='shortcut icon' href='/favicons/favicon.ico' type='image/x-icon' />
+      <link rel='icon' href='/favicons/favicon.png' type='image/png' />
+      <link rel='apple-touch-icon' href='/favicons/apple-touch-icon.png' type='image/png' />
+
+    </Helmet>
+      {children()}
+  </div>
+)
+
+Layout.propTypes = {
+  children: PropTypes.func,
+}
+
+export default Layout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
