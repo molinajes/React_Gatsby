955b32c5240b82e24f629a61fa84a31754d02997
webdevcrazier <devcrazier@gmail.com>
2018-06-04 17:05:43 +0800

    change contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import _ from 'lodash'
 import Link from 'gatsby-link'
-import { Menu, Section, Container } from 'components'
+import { Menu, Section, Container, Contact } from 'components'
 
 const ContactPage = ({ history }) => {
   let href = _.get(history, 'location.pathname')
   return (
-    <Section dark>
+    <div>
       <Menu href={href} />
-      <Container>
-        <h1>Hi from the Contact page</h1>
-        <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
-      </Container>
-    </Section>
+      <Contact />
+    </div>
   );
 }
 
