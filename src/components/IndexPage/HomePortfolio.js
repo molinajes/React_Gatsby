d18f2604ca27c0a5eb95d4e64917a81d6b61abaa
webdevcrazier <devcrazier@gmail.com>
2018-06-04 17:02:28 +0800

    delete homecontact, change home portfolio

--- src/components/IndexPage/HomePortfolio.js
+++ src/components/IndexPage/HomePortfolio.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link'
-import { Error, Section, Container } from 'components'
+import { Error, Section, Container, PortfolioList } from 'components'
 
 import portfolioMock from 'components/portfolioMock'
-
-const PortfolioItem = ({ title, category, imageSrc, href }) => (
-  <div className="list-block-item">
-    <img src={imageSrc} className="list-block-item-image" />
-    <div className="list-block-item-hover">
-      <h4 className="list-block-item-heading">{title}</h4>
-      <p>{category}</p>
-      <a href={href} className="link-with-arrow">Étude de cas</a>
-    </div>
-  </div>
-)
-
-const PortfolioList = ({ data }) => {
-  if (!data || !data.length) return <Error>Nothing to show</Error>
-  return (
-    <div className="list-block">
-      { data.map(item => <PortfolioItem key={item.title} {...item} />) }
-    </div>
-  )
-}
 
 const HomePortfolio = ({...props}) => (
   <Section {...props}>
